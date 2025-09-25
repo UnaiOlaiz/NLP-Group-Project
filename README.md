249ab5107a3740cd94ccbcf70ac047e8a057fd41
Diego <diego.lopez.a@opendeusto.es>
2025-09-25 13:52:14 +0200

    pos tagging terminado

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # NLP Group Project
 
 # Enlaces interesantes con doc
-https://camel-tools.readthedocs.io/en/latest/api/morphology/analyzer.html
+https://camel-tools.readthedocs.io/en/stable/index.html
--- src/prepare_data.py
+++ src/prepare_data.py
@@ -2,17 +2,29 @@ arab_quran = "../data/raw_data/arab_quran.txt"
 eng_quran = "../data/raw_data/english_quran.txt"
 
 import re
-# Mejor normalizar con esta librería que hacerlo a mano
+'''
+Con esto se consigue que palabras que son la misma,
+pero escritas con variantes gráficas, se traten igual.
+'''
 from camel_tools.morphology.analyzer import DEFAULT_NORMALIZE_MAP as normalizer
+
+'''
+elimina los diacríticos árabes (ḥarakāt), es decir,
+las marcas cortas de vocalización y otros símbolos fonéticos.
+
+Asi evitamos que los diacríticos interfieran en el análisis 
+(la mayoría de modelos entrenados de CAMeL Tools usan texto sin diacríticos, y si usamos otros modelos esto puede ayudar).
+'''
+from camel_tools.utils.dediac import dediac_ar
 
 print(normalizer.map_string("بِسْمِ اللَّهِ الرَّحْمَنِ الرَّحِيمِ"))
 
 # Arabic Quran normalization
 with open(arab_quran, "r", encoding="utf-8") as f:
     arabic_lines = f.readlines()
-    arabic_lines = [normalizer.map_string(line) for line in arabic_lines]
+    arabic_lines = [dediac_ar(normalizer.map_string(line)) for line in arabic_lines]
     with open("../data/cleaned_data/cleaned_arab_quran.txt", "w", encoding="utf-8") as out_f:
-        out_f.writelines("\n".join(arabic_lines))
+        out_f.writelines("".join(arabic_lines))
 
 # English Quran normalization
 def normalize_english(text: str) -> str:
